77e3c371fcb0baede3d764e920d24d37d790744a
T0raT <tigerliu@bu.edu>
2024-09-29 18:07:23 -0400

    idek anymore

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Plot from "react-plotly.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(genRanData());
+  const [centroids, setCentroids] = useState([]);
+  const [k, setK] = useState(3);
+
+  const clusterColors = ["red", "green", "orange", "purple", "cyan"];
+
+  function initRandCentroids() {
+    const dataCopy = [...data];
+    let randIndex = 0;
+    let centroids = [];
+    for (let i = 0; i < k; i++) {
+      randIndex = Math.floor(Math.random() * dataCopy.length);
+      centroids.push({
+        id: i,
+        x: dataCopy[randIndex].x,
+        y: dataCopy[randIndex].y,
+      });
+      dataCopy.splice(randIndex, 1);
+    }
+    setCentroids(centroids);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Plot
+      data={[
+        {
+          x: data.map((p) => p.x),
+          y: data.map((p) => p.y),
+          mode: "markers",
+          type: "scatter",
+          marker: { color: "black" },
+        },
+      ]}
+      layout={{ width: 600, height: 600, title: "some graph" }}
+    />
+  );
 }
 
-export default App
+export default App;
+
+function genRanData() {
+  const points = [];
+
+  for (let i = 0; i < 100; i++) {
+    points.push({
+      id: i,
+      x: Math.random() * 11,
+      y: Math.random() * 11,
+      clusterID: null,
+    });
+  }
+  return points;
+}
